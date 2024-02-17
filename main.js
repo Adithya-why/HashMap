a2f5568a67c0968ea2f4e2201b5868b4c3117d6d
Adi <adithiyans123@gmail.com>
2024-02-17 14:14:39 +0530

    created the set method

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function HashMap(){
 
     let hashArray = [];
 
-    
+    //generates a hash code for a given string key
     function hash(key){
         let hashCode = 0;
 
@@ -16,16 +16,58 @@ function HashMap(){
         return hashCode%17;
     }
 
-
+    //insert new element
     function set(key,value){
+        //generate hash
+        code = hash(key);
+        console.log(code);
+        
+
+        //if bucket doesnt exist create bukcet list at location code
+        //and insert the key value
+        if(!hashArray[code]){
+            hashArray[code] = [[key,value]];
+            console.log(hashArray);
+            return
+        }
+
+        else{
+            let bucket = hashArray[code]
+            console.log(bucket);
+            found = false;
+
+            //if bucket exists and the same key entry isfound
+            //then value isupdated
+            for(let i =0;i<bucket.length;i++){
+                if(bucket[i][0]===key){
+                    found = true
+                    bucket[i][1]=value;
+                    console.log("heheh")
+                    console.log(hashArray);
+                    return
+                }
+            }
+
+            //if bucket exists but key doesnt, the insert the new key value into bucket
+
+            if(!found){
+                bucket.push([key.value])
+                return
+            }
+
+
+        }
         
     }
 
 
 
-    return {hash};
+    return {hash,set};
 }
 
 
 hashObj = HashMap();
-console.log(hashObj.hash("adithiyan s"));
+
+hashObj.set("adi","s");
+hashObj.set("adi","a");
+hashObj.set("sfjk","LL")
