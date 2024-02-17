fd17786f819c7065f84abbbaec6e91aa0fd38958
Adi <adithiyans123@gmail.com>
2024-02-17 13:21:05 +0530

    made a hash function

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("HFJJKFHf");
+function HashMap(){
+
+    let hashArray = [];
+
+    
+    function hash(key){
+        let hashCode = 0;
+
+        const primeNum =31;
+        for(let i =0;i<key.length;i++){
+            hashCode+= hashCode*primeNum+ key.charCodeAt(i);
+
+        }
+
+
+        return hashCode%17;
+    }
+
+
+    function set(key,value){
+        
+    }
+
+
+
+    return {hash};
+}
+
+
+hashObj = HashMap();
+console.log(hashObj.hash("adithiyan s"));
